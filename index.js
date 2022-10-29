1ff4d0aba0f7da5d13bf6a7935ae8c43106a9433
Satoe Sakonjo <satoesakonjo@satoe-mbp.local>
2022-10-29 09:13:29 -0700

    Procatice classes

--- index.js
+++ index.js
@@ -1,29 +1,77 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
+
+class Transaction {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+  }
+}
+
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount
+  }
+
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount
+  }
+
+  isAllowed() {
+    return true;
   }
 
 }
 
 
 
-
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(50.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+console.log('Ending Balance:', myAccount.balance);
